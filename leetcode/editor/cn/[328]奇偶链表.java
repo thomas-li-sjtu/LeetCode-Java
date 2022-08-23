63e733ee4f79734d9f1c83707fa6f58fe974f791
thomas-li-sjtu <980226547@qq.com>
2022-08-23 11:40:56 +0800

    done

---
+++ leetcode/editor/cn/[328]奇偶链表.java
@@ -0,0 +1,63 @@
+//给定单链表的头节点 head ，将所有索引为奇数的节点和索引为偶数的节点分别组合在一起，然后返回重新排序的列表。 
+//
+// 第一个节点的索引被认为是 奇数 ， 第二个节点的索引为 偶数 ，以此类推。 
+//
+// 请注意，偶数组和奇数组内部的相对顺序应该与输入时保持一致。 
+//
+// 你必须在 O(1) 的额外空间复杂度和 O(n) 的时间复杂度下解决这个问题。 
+//
+// 
+//
+// 示例 1: 
+//
+// 
+//
+// 
+//输入: head = [1,2,3,4,5]
+//输出: [1,3,5,2,4] 
+//
+// 示例 2: 
+//
+// 
+//
+// 
+//输入: head = [2,1,3,5,6,4,7]
+//输出: [2,3,6,7,1,5,4] 
+//
+// 
+//
+// 提示: 
+//
+// 
+// n == 链表中的节点数 
+// 0 <= n <= 10⁴ 
+// -10⁶ <= Node.val <= 10⁶ 
+// 
+// Related Topics 链表 👍 619 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+package editor.cn;
+
+class Solution328 {
+    public ListNode oddEvenList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        // head 为奇链表头结点，o 为奇链表尾节点
+        ListNode o = head;
+        // p 为偶链表头结点
+        ListNode p = head.next;
+        // e 为偶链表尾节点
+        ListNode e = p;
+        while (o.next != null && e.next != null) {
+            o.next = e.next;
+            o = o.next;
+            e.next = o.next;
+            e = e.next;
+        }
+        o.next = p;
+        return head;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
