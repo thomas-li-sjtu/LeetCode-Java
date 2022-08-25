0b1d4c094f6e448ca96c41422fa0af75e7084eee
thomas-li-sjtu <980226547@qq.com>
2022-08-25 20:22:37 +0800

    done

--- leetcode/editor/cn/[454]四数相加 II.java
+++ leetcode/editor/cn/[454]四数相加 II.java
@@ -47,7 +47,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 //leetcode submit region begin(Prohibit modification and deletion)
-class Solution {
+class Solution454 {
     public int fourSumCount(int[] nums1, int[] nums2, int[] nums3, int[] nums4) {
         Map<Integer, Integer> map_1 = new HashMap<>();
         for (int i : nums1) {
