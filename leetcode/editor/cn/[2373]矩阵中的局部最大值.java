b383640efa5d67cec92a1cad317515754ecdea88
thomas-li-sjtu <980226547@qq.com>
2022-09-01 13:33:04 +0800

    22/8/14周赛1，done

---
+++ leetcode/editor/cn/[2373]矩阵中的局部最大值.java
@@ -0,0 +1,80 @@
+//给你一个大小为 n x n 的整数矩阵 grid 。 
+//
+// 生成一个大小为 (n - 2) x (n - 2) 的整数矩阵 maxLocal ，并满足： 
+//
+// 
+// maxLocal[i][j] 等于 grid 中以 i + 1 行和 j + 1 列为中心的 3 x 3 矩阵中的 最大值 。 
+// 
+//
+// 换句话说，我们希望找出 grid 中每个 3 x 3 矩阵中的最大值。 
+//
+// 返回生成的矩阵。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//
+// 
+//输入：grid = [[9,9,8,1],[5,6,2,6],[8,2,6,4],[6,2,2,2]]
+//输出：[[9,9],[8,6]]
+//解释：原矩阵和生成的矩阵如上图所示。
+//注意，生成的矩阵中，每个值都对应 grid 中一个相接的 3 x 3 矩阵的最大值。 
+//
+// 示例 2： 
+//
+// 
+//
+// 
+//输入：grid = [[1,1,1,1,1],[1,1,1,1,1],[1,1,2,1,1],[1,1,1,1,1],[1,1,1,1,1]]
+//输出：[[2,2,2],[2,2,2],[2,2,2]]
+//解释：注意，2 包含在 grid 中每个 3 x 3 的矩阵中。
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// n == grid.length == grid[i].length 
+// 3 <= n <= 100 
+// 1 <= grid[i][j] <= 100 
+// 
+// Related Topics 数组 矩阵 👍 8 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+package editor.cn;
+
+class Solution2373 {
+    public int[][] largestLocal(int[][] grid) {
+        int[][] rowGrid = new int[grid.length][grid[0].length-2];
+
+        for (int i = 0; i < grid.length; i++) {
+            int[] row = grid[i];
+            for (int j = 0; j < row.length-2; j++) {
+                rowGrid[i][j] = threeMax(grid[i][j], grid[i][j+1], grid[i][j+2]);
+            }
+        }
+
+        int[][] res = new int[rowGrid.length-2][rowGrid[0].length];
+
+        for (int i = 0; i < rowGrid.length-2; i++ ) {
+            for (int j = 0; j < rowGrid[0].length; j++) {
+                res[i][j] = threeMax(rowGrid[i][j], rowGrid[i+1][j], rowGrid[i+2][j]);
+            }
+        }
+        return res;
+    }
+
+    public int threeMax(int a, int b, int c) {
+        int max = a;
+        if (max < b)
+            max = b;
+        if (max < c)
+            max = c;
+        return max;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
