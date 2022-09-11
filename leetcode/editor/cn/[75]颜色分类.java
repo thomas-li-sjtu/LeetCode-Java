9d93c9f8e7faf41e419dd668efce8c74a4cc4629
thomas-li-sjtu <980226547@qq.com>
2022-09-11 16:40:52 +0800

    done

--- leetcode/editor/cn/[75]颜色分类.java
+++ leetcode/editor/cn/[75]颜色分类.java
@@ -67,6 +67,22 @@ class Solution75 {
                 ++ptr;
             }
         }
+        // int n = nums.length;
+        // int p0 = 0, p2 = n - 1;
+        // for (int i = 0; i <= p2; ++i) {
+        //     while (i <= p2 && nums[i] == 2) {
+        //         int temp = nums[i];
+        //         nums[i] = nums[p2];
+        //         nums[p2] = temp;
+        //         --p2;
+        //     }
+        //     if (nums[i] == 0) {
+        //         int temp = nums[i];
+        //         nums[i] = nums[p0];
+        //         nums[p0] = temp;
+        //         ++p0;
+        //     }
+        // }
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
