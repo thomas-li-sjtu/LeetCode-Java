cbedf68cba2f808316587f037f26e9f547fcdfd8
thomas-li-sjtu <980226547@qq.com>
2022-09-22 19:20:37 +0800

    需要重做

---
+++ leetcode/editor/cn/[560]和为 K 的子数组.java
@@ -0,0 +1,51 @@
+//给你一个整数数组 nums 和一个整数 k ，请你统计并返回 该数组中和为 k 的连续子数组的个数 。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：nums = [1,1,1], k = 2
+//输出：2
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：nums = [1,2,3], k = 3
+//输出：2
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= nums.length <= 2 * 10⁴ 
+// -1000 <= nums[i] <= 1000 
+// -10⁷ <= k <= 10⁷ 
+// 
+// Related Topics 数组 哈希表 前缀和 👍 1661 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+package editor.cn;
+
+import java.util.*;
+
+class Solution560 {
+    public int subarraySum(int[] nums, int k) {
+        int count = 0, pre = 0;
+        HashMap < Integer, Integer > mp = new HashMap < > ();
+        mp.put(0, 1);
+        for (int i = 0; i < nums.length; i++) {
+            pre += nums[i];
+            if (mp.containsKey(pre - k)) {
+                count += mp.get(pre - k);
+            }
+            mp.put(pre, mp.getOrDefault(pre, 0) + 1);
+        }
+        return count;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
